1b758386edb7c372c4de510cbb3582f9ea7fc02d
jeongmin <tmxk0810@gmail.com>
2022-04-22 12:23:45 +0900

    레이아웃을 RelativeLayout 이 감싸는 형태로 싹 바꿨는데도 캡쳐가 안됨ㅜㅜ 잠시 후에 다시 시도 할 것...

--- app/src/main/java/com/example/lotto/MainActivity.java
+++ app/src/main/java/com/example/lotto/MainActivity.java
@@ -2,10 +2,24 @@ package com.example.lotto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Calendar;
@@ -17,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
                 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
                 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
                 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-                41, 42, 43, 44, 45, 46}; // 1~45까지의 숫자 배열 지정
+                41, 42, 43, 44, 45}; // 1~45까지의 숫자 배열 지정
 
     int[] lottoRandom = new int[6]; // 한 회에 6번씩 나오게 할 거기 때문에 6사이즈의 배열 <- 이게 다섯 개 있어야 함(5줄)
 
@@ -31,8 +45,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        checkPermission();
         initNum();
-        random();
+
     }
 
     public void initNum() {
@@ -63,7 +78,15 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void random() {
+    public void onClickCapture(View view) {
+
+        RelativeLayout rlCaptureLayout = (RelativeLayout) findViewById(R.id.rlCaptureLayout); // 캡쳐할 영역의 레이아웃을 가져옴
+        SimpleDateFormat sdf = new SimpleDateFormat("yy/M/d_E"); // 연도/월/일_요일 로 파일명 지정
+        LayoutCapture(rlCaptureLayout, sdf.format(new Date()));
+
+    }
+
+    public void random() { // 레이아웃 다 바꾸고 실행하면 여기가 문제가 됩니다..
 
         TextView tvNumberArray = (TextView) findViewById(R.id.tvNumberArray);
 
@@ -77,13 +100,76 @@ public class MainActivity extends AppCompatActivity {
         lottoRandom = Arrays.copyOf(ran, 30); // lottoRandom 은 ran 배열을 복사한 배열
         for(int i = 0; i<5; i++ ){
             tvNumberArray.append(alphabet[i] + " 자 동 ");
-            for(int j=0; j<6; j++) {
+            for(int j=0; j<lottoRandom.length; j++) {
+                Log.i("jeongmin", "j 의 값 : " + j);
+                if(j % 5 == 0) {
+                    tvNumberArray.append("\n");
+                    Log.i("jeongmin", "5로 나눴을 때 나머지 0임!!! j 값 : " + j);
+                    Log.i("jeongmin", "lottoRandom[" + j + "] : " + lottoRandom[j]);
+                }
                 tvNumberArray.append(lottoRandom[j] + "  ");
+                // 22. 4. 21(목) 수정 중인데 됐다 안됐다 난리..
             }
             tvNumberArray.append("\n\n"); // 한 줄 하고 두 줄 띄우기
         }
 
     }
+
+    public void checkPermission() {
+        // storage 접근 권한 확인
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                    || checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                if(shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                    Toast.makeText(this, "외부 저장소 사용을 위해 읽기/쓰기 필요", Toast.LENGTH_SHORT).show();
+                }
+
+                requestPermissions(new String[]
+                        {Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE}, 2);
+            }
+        }
+    }
+
+
+    // LayoutCapture 클래스 생성
+    public void LayoutCapture(View view, String title) {
+        if(view == null) {
+            Log.i("jeongmin", "<LayoutCapture 클래스 안> 뷰가 비어있을 경우의 예외처리");
+            Log.i("jeongmin", "<LayoutCapture 클래스 안>view is null!!");
+            return;
+        }
+        Log.i("jeongmin", "<LayoutCapture 클래스 안>캡쳐 파일 저장 영역");
+        Log.i("jeongmin", "<LayoutCapture 클래스 안>캐시 비트맵 만들기");
+        view.buildDrawingCache();
+        Bitmap bitmap = view.getDrawingCache();
+        FileOutputStream fos = null;
+
+        // 파일 저장
+        String strPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).toString()+ "/lotto";
+
+        Log.i("jeongmin", "<LayoutCapture 클래스 안>경로 : " + strPath);
+
+        File uploadFolder = new File(strPath);
+        Log.i("jeongmin", "<LayoutCapture 클래스 안>업로드 경로 설정 : view_capture 라는 폴더 생성해서 그 폴더에 저장 하려고 위에처럼 경로 설정" + uploadFolder);
+
+        String fileName = title + ".jpg";
+
+        File tempFile = new File(strPath, fileName);
+
+        try {
+            if(!uploadFolder.exists()) { // 만약 지정 폴더가 없으면 생성
+                uploadFolder.mkdirs();
+            }
+
+            fos = new FileOutputStream(tempFile); // 경로 + 제목 + .jpg 로 FileOutputStream Setting
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+            Toast.makeText(getApplicationContext(), "갤러리 저장에 성공했습니다.", Toast.LENGTH_SHORT).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Log.i("jeongmin", "<try - catch 의 catch 문 속>");
+            Log.i("jeongmin", "파일을 찾을 수 없습니다.");
+        }
+        // 이미지 스캐닝 해서 갤러리에서 보이게 해 주는 코드
+        sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(tempFile)));
+    }
 }
-// 30개의 숫자가 다 다르게 나오게 하는 건 고민해보고..내일...
-// 나눔 Lotto 옆에 버튼 하나 추가해서 화면 레이아웃 갤러리에 저장할 수 있는 버튼 생성하기
